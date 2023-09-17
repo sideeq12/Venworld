4d81374cdc41bcb982d689c22e015c85f3a5a4c1
sideeq12 <abdwaheed2018@gmail.com>
2023-09-17 10:40:27 +0100

    layout

--- components/blog/blog.jsx
+++ components/blog/blog.jsx
@@ -16,10 +16,11 @@ function Blog() {
                 Congratulations are in order to our Head of Graphics Designer at VenWorld Global (Josiah Damilare)
                 Honored as PROLIFIC GRAPHIC DESIGNER
                 </h3>
-                <p className='text-sm'> Meet Josiah also known as eternal, prolific designer with over decade of experience in industry
+                <p className='text-sm'> Meet Josiah also 
+                known as eternal, prolific designer with over decade of experience in industry
                     <a> Read more</a> </p>
                     <div className='text-sm mt-1 md:mt-2'> <button
-                      className='px-2 py-1 bg-slate-100 mr-3 text-black rounded-sm'>  Learn more</button>
+                      className='px-2 py-1 bg-slate-100 mr-3 text-black rounded-sm'>  Read more</button>
                         <span className='mr-3 text-sm'> April 20, 2021</span>
                      </div>
             </div>
@@ -28,14 +29,22 @@ function Blog() {
                 <div className='h-48 border border-cyan second relative rounded-lg mb-10'>
                     <div className='absolute bottom-0 font-bold text-white px-3 pb-4'>
                     We research, build and design the design the best tools, to make your web development idea
-                    becomes reality, with branded .. 
-                    <button className='px-1 py-1 bg-slate-100 mr-3 text-black text-sm rounded-sm'>Learn more</button>
+                    becomes reality, with branded
+                    
+                    <div className='text-sm mt-1 md:mt-2'> <button
+                      className='px-2 py-1 bg-slate-100 mr-3 text-black rounded-sm'>  Read more</button>
+                        <span className='mr-3 text-sm'> April 20, 2021</span>
+                     </div>
                     </div>
                 </div>
                 <div className='h-48 rounded-lg border border-cyan mt-2 third relative mb-0'>
-                <div className='absolute bottom-0 text-white px-3 pb-4'>
-                Venworld Global is in affiliation with Golden Signature Advertising LLC Dubai, United Arab Emirates for .......
-                    <button className='px-1 py-1 bg-slate-100 mr-3 text-black text-sm rounded-sm'>Learn more</button>
+                <div className='absolute bottom-0 text-white px-3 pb-4 font-bold'>
+                Venworld Global is in affiliation with Golden Signature 
+                Advertising LLC Dubai, United Arab Emirates 
+                  <div className='text-sm mt-1 md:mt-2'> <button
+                      className='px-2 py-1 bg-slate-100 mr-3 text-black rounded-sm'>  Read more</button>
+                        <span className='mr-3 text-sm'> April 20, 2021</span>
+                     </div>
                     </div>
                     </div>
             </div>
