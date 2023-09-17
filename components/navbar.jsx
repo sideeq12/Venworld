b289efb9c834f9d50a3b85abfafc72713c5bda17
sideeq12 <abdwaheed2018@gmail.com>
2023-09-17 09:26:06 +0100

    radius and text fix

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -18,17 +18,17 @@ function Navbar() {
             {/* <div className='logo fixed'></div> */}
               {status?
               <svg xmlns="http://www.w3.org/2000/svg" 
-              width="30" height="30" fill="currentColor" 
+              width="30" height="30" fill="#000" 
               class="bi bi-x-lg" viewBox="0 0 16 16">           
               <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
             </svg> 
-            :  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            :  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000" class="bi bi-list" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
 </svg>}
             </div>
 
           <div  className={status ? `menu ` : `menux`}>
-            <ul>
+            <ul className='text-blue-900'>
               <li><Link  href="#">HOME</Link></li>
               <li><Link href="#">ABOUT US</Link></li>
               <li><Link href="#">SERVICES</Link></li>
